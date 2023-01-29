b9514004645ed85d309e794ddcd47edfc0f58fbb
BelleZarocha <91052520+SAROCHA042@users.noreply.github.com>
2023-01-29 19:10:54 +0700

    edit

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Container, GridArea, ProfileBox } from "../StylesPages/PageLayout"
 import './Profile.css'
 
 function Profile() {
-  return (
-    <Container>
-        <GridArea>
-            <ProfileBox>
-                <img src="https://drive.google.com/uc?id=1fyf7UZ-rJBZrRA5TR_UY6_-OMsazr1xf&authuser=0" style={{height: "80px", margin: "20px"}} />
-            </ProfileBox>
+    const [data, setData] = useState([]);
 
-            <ProfileBox>
+    useEffect(() => {
+        async function fetchData() {
+        const response = await axios.get('https://dull-blue-gorilla-shoe.cyclic.app/user/edit');
+        setData(response.data);
+    }
+    fetchData();
+    }, []);
+    return (
+        <Container>
+            <GridArea>
+                <ProfileBox>
+                    <img src="https://drive.google.com/uc?id=1fyf7UZ-rJBZrRA5TR_UY6_-OMsazr1xf&authuser=0" style={{height: "80px", margin: "20px"}} />
+                    {data.map(item => (
+                    <div key={item.id}> 
+                        <h2>Username: {item.username}</h2>
+                        
+                        <div style={{fontSize: "20px"}}>
+                        
+                        
+                        </div>
+                    </div>
+                    ))}
+                </ProfileBox>
 
-            </ProfileBox>
-        </GridArea>
-        <div style={{display: "flex", justifyContent: "center"}}>
-            <a href="/edit_profile" className='editBtn'>Edit Profile</a>
-        </div>
-    </Container>
-  )
+                <ProfileBox>
+
+                </ProfileBox>
+            </GridArea>
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <a href="/edit_profile" className='editBtn'>Edit Profile</a>
+            </div>
+        </Container>
+    )
 }
 
 export default Profile
